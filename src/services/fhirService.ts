55c394f66a4ee3737fbc58e25876ee291d03a919
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-04-24 17:07:52 -0500

    Funcional

--- src/services/fhirService.ts
+++ src/services/fhirService.ts
@@ -1,7 +1,13 @@
 // src/fhirService.ts
 import Client from 'fhir-kit-client';
 
-const client = new Client({ baseUrl: 'http://hii1sc-dev.inf.pucp.edu.pe/openmrs/ws/fhir2/R4' });
+
+const client = new Client({
+    baseUrl: 'http://hii1sc-dev.inf.pucp.edu.pe/openmrs/ws/fhir2/R4',
+    customHeaders: {
+      Authorization: 'Basic ' + Buffer.from('admin:Admin123').toString('base64'),
+    },
+  });
 
 const isPatient = (resource: fhir4.Resource): resource is fhir4.Patient => {
   return resource.resourceType === 'Patient';
