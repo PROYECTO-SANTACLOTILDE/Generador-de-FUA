d6e1f5b7033098cf5a12f0052bd48fcd486ea74d
EvanLin46 <linares.evan.yz@gmail.com>
2025-09-01 12:05:31 -0500

    added enums

--- src/middleware/logger/models/typescript/EnvironmentType.ts
+++ src/middleware/logger/models/typescript/EnvironmentType.ts
@@ -15,3 +15,15 @@
 
 
 // Export Methods
+
+
+export enum Logger_EnvironmentType {
+    // Development environment logs
+	TRACE = 'DEV',
+	// Quality assurance / testing environment logs
+    DEBUG = 'QUALITY',
+	// Production environment logs (real system usage)
+    INFO = 'PROD'
+}
+
+
---
+++ src/middleware/logger/models/typescript/LogType.ts
@@ -0,0 +1,12 @@
+
+export enum Logger_LogType {
+    // A new resource was created
+	CREATE = 'CREATE',
+	// A resource was accessed or consulted
+    READ = 'READ',
+	// A file or data was uploaded
+    UPLOAD = 'UPLOAD',
+	// A resource was deleted
+    DELETE = 'DELETE'
+}
+
